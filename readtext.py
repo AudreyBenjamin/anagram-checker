81cd648a7adffbc471b3ecfbdc0b55ce9d9d293c
audrey benjamin <ugoezeoffordile@gmail.com>
2022-05-28 21:27:57 +0100

    Add count words function

---
+++ readtext.py
@@ -0,0 +1,20 @@
+from itertools import count
+from posixpath import split
+
+
+def read_file_content(filename):
+    count_words = {}
+    with open(filename, "r") as openfile:
+        read_file = openfile.read()
+        words = read_file.split()
+      
+        for word in words:
+            if word in count_words:
+                count_words[word] +=1
+            else:
+                count_words[word] = 1
+    return count_words
+
+
+     
+print (read_file_content("./story.txt"))
